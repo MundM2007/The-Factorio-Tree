257c047f079ba1d1c22b048fa3606cfbb6bf9763
MundM2007 <schaefer781@gmx.de>
2025-06-13 18:36:35 +0200

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -13,8 +13,8 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.1.0",
-	name: "Red Science",
+	num: "0.1.1",
+	name: "",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
